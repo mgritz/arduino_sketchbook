38d4b15296841e73dc8501e50daf3e41fc464a73
Martin Gritzan <martin.gritzan@indurad.com>
2017-04-30 22:10:43 +0200

    WIP: first working version
    arduino only understands PULLUP resistors...

--- matrix_keyboard/matrix_keyboard.ino
+++ matrix_keyboard/matrix_keyboard.ino
@@ -5,22 +5,41 @@ Matrix Keyboard
 
 */
 
+#define NUM_ROWS 4
+#define NUM_COLS 4
+
 // interface pins for matrix display
-const int rowPins[] = {20, 22, 24, 26};
-const int colPins[] = {28, 30, 32, 34};
+const int rowPins[NUM_ROWS] = {22, 24, 26, 28};
+const int colPins[NUM_COLS] = {30, 32, 34, 36};
+const char keycodes[NUM_ROWS][NUM_COLS]= {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
 
 void setup() {
   
   Serial.begin(9600);
   
-  for (int i = 0; i < 4; ++i) {
+  for (int i = 0; i < NUM_ROWS; ++i)
+  {
     pinMode(rowPins[i], OUTPUT);
-    pinMode(colPins[i], INPUT);
+    digitalWrite(rowPins[i], HIGH);
   }
-  
+  for (int i = 0; i < NUM_COLS; ++i)
+    pinMode(colPins[i], INPUT_PULLUP);  
 }
 
 void loop() {
-  
+  for (int row = 0; row < NUM_ROWS; row++)
+  {
+    digitalWrite(rowPins[row], LOW);
+    for (int col = 0; col < NUM_COLS; col++)
+      if (digitalRead(colPins[col]) == LOW)
+        Serial.print(keycodes[row][col]);
+        
+    digitalWrite(rowPins[row], HIGH);
+  }
   
 }
